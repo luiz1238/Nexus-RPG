8cf4bc4457f502ff8d2d01a6e96f1fc46227ae17
João Pedro Lira <126631781+Shisuiicaro@users.noreply.github.com>
2026-05-13 16:50:14 -0300

    Update PortraitAvatarContainer.tsx

--- src/components/Portrait/PortraitAvatarContainer.tsx
+++ src/components/Portrait/PortraitAvatarContainer.tsx
@@ -97,6 +97,8 @@ export default function PortraitAvatar(props: {
     <Fade in={showAvatar}>
       <div
         style={{
+          width: 420,
+          height: 600,
           filter: diceColor ? `drop-shadow(0 0 15px #${diceColor})` : 'none',
         }}
       >
